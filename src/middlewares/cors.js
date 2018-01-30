9a49bf3799e5d557bb47ef8df1cb25101444ed84
kyotaw <httg1326@gmail.com>
2018-01-31 02:12:17 +0900

    add res header

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -5,6 +5,7 @@ function allowCORS(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, HEAD, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 }
 
