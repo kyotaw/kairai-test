9672a78c01f11defb83f1c51b99ca2a25986c223
kyotaw <httg1326@gmail.com>
2018-02-13 01:55:20 +0900

    fixed bugs

--- src/models/channel_average.js
+++ src/models/channel_average.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const ChannelAggregation = require('./channel_aggregation').ChannelAggregation
-    , average = require('../helpers/statistics').average;
+    , average = require('../helpers/statistics').average
+    , timestamp = require('../helpers/time').timestamp;
 
 class ChannelAverage extends ChannelAggregation {
 
@@ -12,6 +13,7 @@ class ChannelAverage extends ChannelAggregation {
         const values = dataArray.map(d => { return d.toArray(); });
         const ave = average(values);
         let aveData = new dataArray[0].constructor(ave);
+        aveData.timestamp = timestamp();
         aveData.samples = dataArray.length;
         return aveData;
     }
--- src/ws.listener.s.test.js
+++ src/ws.listener.s.test.js
@@ -2,15 +2,15 @@
 
 const socketio = require('socket.io-client');
 
-//const listener = socketio('https://kairai.herokuapp.com/aggregation', {
-const listener = socketio('http://localhost:6171/aggregation', {
+const listener = socketio('https://kairai.herokuapp.com/aggregation', {
+//const listener = socketio('http://localhost:6171/aggregation', {
     query: {
         path: '/api/data_stream/',
         dataSourceType: 'barometer',
         latitude: 35.7007777,
-        longitude: 170.71475,
+        longitude: 139.71475,
         radius: '200', // km
-        method: 'average'
+        method: 'raw'
     }
 });
 
