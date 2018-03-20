20acbd48e02368f9fe3d09511d5d4976755dad70
kyotaw <httg1326@gmail.com>
2018-03-21 02:34:44 +0900

    improved layout

--- src/ws.listener.test.js
+++ src/ws.listener.test.js
@@ -6,7 +6,7 @@ const io = socketio('http://localhost:6171/direct', {
 //const io = socketio('https://kairai.herokuapp.com/direct', {
     query: {
         path: '/api/data_stream',
-        dataSourceHash: '324ff6cdefd9571fcef991516dc0f745cd1b73c9a017586bdb43fc13f50ce0ce74dcea26a5c2be537ad6808ba66563b4c113ca3b9ebcc98b6ae933a788dc79e5'
+        dataSourceHash: '5c11bae2db2a33fac32249c4b410bb1d34d247202040ec6e5df8b517e67eaa98a161439f0d910065cf2877be9f647b190786cf98c39d6f0160fe0ddfce3c7170'
     }
 });
 
